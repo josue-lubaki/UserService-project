d1dca7d4cf7b7b13f46276ccf6460c754e7d816f
Josue Lubaki <josuelubaki@gmail.com>
2022-03-12 01:54:20 -0500

    config authorizationFilter

--- src/main/java/ca/josue/userservice/security/SecurityConfig.java
+++ src/main/java/ca/josue/userservice/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ca.josue.userservice.security;
 
 import ca.josue.userservice.filter.CustomAuthentificationFilter;
+import ca.josue.userservice.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -46,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/v1/users/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthentificationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
