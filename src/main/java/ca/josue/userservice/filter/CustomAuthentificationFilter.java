01716ef55555a7cc7209246126d89a202db1fda7
Josue Lubaki <josuelubaki@gmail.com>
2022-03-12 00:14:57 -0500

    config successfulAuthentication

--- src/main/java/ca/josue/userservice/filter/CustomAuthentificationFilter.java
+++ src/main/java/ca/josue/userservice/filter/CustomAuthentificationFilter.java
@@ -1,10 +1,14 @@
 package ca.josue.userservice.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 /**
  * @author Josue Lubaki
@@ -33,12 +39,40 @@ public class CustomAuthentificationFilter extends UsernamePasswordAuthentication
         log.info("Userame is : {}", username); log.info("Password is : {}", password);
 
         // initialise a UsernamePasswordAuthenticationToken which receive a username and a password.
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authentication) throws IOException, ServletException {
+
+        // retrieve a User of ( org.springframework.security.core.userdetails)
+        User user = (User)authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("my-secret-word".getBytes());
+        String accessToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * 60 * 10)))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * 60 * 60)))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .sign(algorithm);
+
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refresh_token);
     }
 }
